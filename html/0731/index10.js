73321f9a344772d0fd809df74204d3be6c9fb1a2
kungjin <vudchsdl123@gmail.com>
2025-08-01 16:28:47 +0900

    수정사항 0801
    제어문 쪽 링크 수정

---
+++ html/0731/index10.js
@@ -0,0 +1,24 @@
+let memNum = 49;
+let colNum = 6;
+let rowNum;
+
+if (memNum % colNum == 0) {
+    rowNum = memNum / colNum;
+} // 행열의 갯수
+
+else { //저게 나머지가 안남는게 아니라 남는다면 
+    rowNum = parseInt(memNum / colNum) + 1;
+} // 줄에서 나머지가 생긴다면 하나의 줄을 더 생성해야 하는 부분을 표기 
+document.writeln(`<table>`);
+for (let i = 0; i < rowNum; i++) {
+    document.writeln(`<tr>`);
+    for (let j = 1; j <= colNum; j++) {
+        seatNo = i * colNum + j;
+        if (seatNo > memNum) break;
+        document.writeln(`<td> 좌석 ${seatNo}</td>`);
+    }
+    
+    document.writeln(`</tr>`)
+
+}
+document.writeln(`</table>`);
